8f7f62d585900a50074d6ff8000cbadc8d7e861f
Sanj2809 <dangvanngai57@gmail.com>
2026-04-03 23:23:12 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,8 +14,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Check role
 def has_permission(member: discord.Member):
-    user_roles = [role.name for role in member.roles]
-    return any(role in ALLOWED_ROLES for role in user_roles)
+    return discord.utils.get(member.roles, name="Helper") is not None
 
 @bot.event
 async def on_ready():
@@ -31,24 +30,17 @@ async def on_ready():
 @bot.tree.command(name="stk", description="Gửi QR thanh toán")
 async def stk(interaction: discord.Interaction):
 
-    # Check quyền
     if not has_permission(interaction.user):
         await interaction.response.send_message(
-            "❌ Bạn không có quyền dùng lệnh này!",
+            "❌ Chỉ Helper mới dùng được!",
             ephemeral=True
         )
         return
 
-    try:
-        file = discord.File("qr.png")  # file ảnh trong thư mục
-        await interaction.response.send_message(
-            content="💸 Quét mã để thanh toán",
-            file=file
-        )
-    except Exception as e:
-        await interaction.response.send_message(
-            f"❌ Lỗi gửi ảnh: {e}",
-            ephemeral=True
-        )
+    file = discord.File("qr.png")
+    await interaction.response.send_message(
+        content="💸 Quét mã để thanh toán",
+        file=file
+    )
 
 bot.run(TOKEN)
